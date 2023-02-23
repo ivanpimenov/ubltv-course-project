1ca2c80f1fe4e03354e4ec7a7951398d90475b6c
Ivan <ivan.pimenov@psylaris.com>
2023-02-23 17:33:56 +0100

    fix build errors

--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -1,3 +1,6 @@
+import type { UserScheme, User } from './model/types/User'
+
 export { getUserAuthData } from './model/selectors/getUserAuthData/getUserAuthData'
 export { userActions, userReducer } from './model/slice/userSlice'
-export { UserScheme, User } from './model/types/User'
+
+export { UserScheme, User }
