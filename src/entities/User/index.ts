5109575ce09b8f9418124cffcb398f4d5a87bedf
Ivan <ivan.pimenov@psylaris.com>
2023-02-23 18:31:03 +0100

    fix build bug

--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -1,6 +1,4 @@
-import type { UserScheme, User } from './model/types/User'
+export type { UserScheme, User } from './model/types/user'
 
 export { getUserAuthData } from './model/selectors/getUserAuthData/getUserAuthData'
 export { userActions, userReducer } from './model/slice/userSlice'
-
-export { UserScheme, User }
