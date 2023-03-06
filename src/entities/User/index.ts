b15c224b68c0b2fc8ea447b5957b678b28c8e581
Ivan <ivan.pimenov@psylaris.com>
2023-03-06 17:10:15 +0100

    test-fix

--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -1,4 +1,4 @@
-export type { UserScheme, User } from './model/types/user'
+export type { UserScheme, User } from './model/types/User'
 
 export { getUserAuthData } from './model/selectors/getUserAuthData/getUserAuthData'
 export { userActions, userReducer } from './model/slice/userSlice'
