d22a25ad7cf67199f4cb194711547f123a1a6ae1
Ivan <ivan.pimenov@psylaris.com>
2023-03-08 19:10:36 +0100

    add tests for login module

---
+++ src/features/AuthByUserName/model/slice/loginSlice.test.ts
@@ -0,0 +1,16 @@
+import { DeepPartial } from '@reduxjs/toolkit'
+import { loginByUsername } from '../services/loginByUsername/loginByUsername'
+import { LoginSchema } from '../types/LoginSchema'
+import { loginActions, loginReducer } from './loginSlice'
+
+describe('loginSlice', () => {
+    test('set username', () => {
+        const state: DeepPartial<LoginSchema> = { username: 'user' }
+        expect(loginReducer(state as LoginSchema, loginActions.setUsername('admin'))).toEqual({ username: 'admin' })
+    })
+
+    test('set password', () => {
+        const state: DeepPartial<LoginSchema> = { password: '123' }
+        expect(loginReducer(state as LoginSchema, loginActions.setPassword('12345'))).toEqual({ password: '12345' })
+    })
+})
