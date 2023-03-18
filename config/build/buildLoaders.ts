e1d90193a1f9c9f4e01737a89f78ff01d7bffad3
Ivan <ivan.pimenov@psylaris.com>
2023-03-18 08:47:35 +0100

    add new theme, add react refresh plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,8 +1,10 @@
-import webpack from 'webpack';
-import { buildCssLoader } from './loaders/buildCssLoader';
-import { BuildOptions } from './types/config';
+import webpack from 'webpack'
+import { buildBabelLoader } from './loaders/buildBabelLoader'
+import { buildCssLoader } from './loaders/buildCssLoader'
+import { BuildOptions } from './types/config'
 
-export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
+    const { isDev } = options
     const cssLoader = buildCssLoader(isDev)
 
     // Если не используем тайпскрип - нужен babel-loader
@@ -10,7 +12,7 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
-    };
+    }
 
     // webpack v5
 
@@ -35,27 +37,9 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     const svgLoader = {
         test: /\.svg$/,
         use: ['@svgr/webpack'],
-    };
+    }
 
-    const babelLoader = {
-        test: /\.(js|jsx|tsx)$/,
-        exclude: /node_modules/,
-        use: {
-            loader: 'babel-loader',
-            options: {
-                presets: ['@babel/preset-env'],
-                plugins: [
-                    [
-                        'i18next-extract',
-                        {
-                            locales: ['ru', 'en'],
-                            keyAsDefaultValue: true,
-                        },
-                    ],
-                ],
-            },
-        },
-    };
+    const babelLoader = buildBabelLoader(options)
 
     const fileLoader = {
         test: /\.(png|jpe?g|gif|woff2|woff)$/i,
@@ -64,7 +48,7 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
                 loader: 'file-loader',
             },
         ],
-    };
+    }
 
     const fontLoader = {
         test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
@@ -77,8 +61,8 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
                 },
             },
         ],
-    };
+    }
 
-    return [fileLoader, svgLoader, babelLoader, typescriptLoader, cssLoader];
+    return [fileLoader, svgLoader, babelLoader, typescriptLoader, cssLoader]
     // return [fileLoader, svgLoader, typescriptLoader, cssLoader, fontLoader]
 }
--- src/shared/contexts/theme-context/useTheme.ts
+++ src/shared/contexts/theme-context/useTheme.ts
@@ -1,5 +1,5 @@
-import { useContext } from 'react';
-import { LOCAL_STORAGE_THEME, Theme, ThemeContext } from './ThemeContext';
+import { useContext } from 'react'
+import { LOCAL_STORAGE_THEME, Theme, ThemeContext } from './ThemeContext'
 
 interface UseThemeResult {
     toggleTheme: () => void
@@ -7,14 +7,29 @@ interface UseThemeResult {
 }
 
 export function useTheme(): UseThemeResult {
-    const { theme, setTheme } = useContext(ThemeContext);
+    const { theme, setTheme } = useContext(ThemeContext)
 
     const toggleTheme = () => {
-        const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK;
-        setTheme?.(newTheme);
+        // const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK;
+        let newTheme: Theme
+        switch (theme) {
+        case Theme.DARK:
+            newTheme = Theme.LIGHT
+            break
+        case Theme.LIGHT:
+            newTheme = Theme.ORANGE
+            break
+        case Theme.ORANGE:
+            newTheme = Theme.DARK
+            break
+        default:
+            newTheme = Theme.LIGHT
+            break
+        }
+        setTheme?.(newTheme)
         document.body.className = newTheme
-        localStorage.setItem(LOCAL_STORAGE_THEME, newTheme);
-    };
+        localStorage.setItem(LOCAL_STORAGE_THEME, newTheme)
+    }
 
-    return { theme: theme || Theme.LIGHT, toggleTheme };
+    return { theme: theme || Theme.LIGHT, toggleTheme }
 }
