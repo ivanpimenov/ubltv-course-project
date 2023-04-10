66eb9933004c44e20fca7f6641c7ba1de42f7b0f
Ivan <ivan.pimenov@psylaris.com>
2023-04-10 15:52:36 +0200

    add netlify config / fix api config

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,11 +1,14 @@
-import axios from "axios";
-import { USER_LOCALSTORAGE_KEY } from "shared/const/localstorage";
+import axios from 'axios'
+import { USER_LOCALSTORAGE_KEY } from 'shared/const/localstorage'
 
 // const baseUrl = __IS_DEV__ ? 'http://localhost:8000' : 'https://prod.com'
 
 export const $api = axios.create({
     baseURL: __API__,
-    headers: {
-        authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY) || ''
-    }
+})
+
+$api.interceptors.request.use(config => {
+    if (config.headers) config.headers.Authorization = localStorage.getItem(USER_LOCALSTORAGE_KEY) || ''
+
+    return config
 })
