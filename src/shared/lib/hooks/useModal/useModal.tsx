9b9cc01f726eadcd959ce9055b7cb8fbbe972d02
Ivan <ivan.pimenov@psylaris.com>
2023-04-25 18:14:41 +0200

    add useModal hook, upgrade drawer

---
+++ src/shared/lib/hooks/useModal/useModal.tsx
@@ -0,0 +1,63 @@
+import { MutableRefObject, useCallback, useEffect, useRef, useState } from 'react'
+
+interface UseModalProps {
+    onClose?: () => void
+    isOpen?: boolean
+    animationDelay: number
+}
+
+export function useModal({ animationDelay, isOpen, onClose }: UseModalProps) {
+    const [isOpening, setIsOpening] = useState(false)
+    const [isClosing, setIsClosing] = useState(false)
+    const [isMounted, setIsMounted] = useState(false)
+    const timeRef = useRef() as MutableRefObject<ReturnType<typeof setTimeout>>
+
+    const close = useCallback(() => {
+        if (onClose) {
+            setIsClosing(true)
+            timeRef.current = setTimeout(() => {
+                onClose()
+                setIsClosing(prev => !prev)
+            }, animationDelay)
+        }
+    }, [onClose, animationDelay])
+
+    // Новые ссылки !!!
+    const onKeyDown = useCallback(
+        (e: KeyboardEvent) => {
+            if (e.key === 'Escape') close()
+        },
+        [close]
+    )
+
+    // const onContentClick = (e: React.MouseEvent) => e.stopPropagation()
+
+    useEffect(() => {
+        if (isOpen) {
+            window.addEventListener('keydown', onKeyDown)
+            timeRef.current = setTimeout(() => {
+                setIsOpening(true)
+            }, 0)
+        }
+        return () => {
+            setIsOpening(false)
+            clearTimeout(timeRef.current)
+            window.removeEventListener('keydown', onKeyDown)
+        }
+    }, [isOpen, onKeyDown])
+
+    useEffect(() => {
+        if (isOpen) {
+            setIsMounted(true)
+        }
+        // При размонтировании - убираем Portal из DOM
+        return () => setIsMounted(false)
+    }, [isOpen])
+
+    return {
+        isClosing,
+        isOpening,
+        isMounted,
+        close,
+    }
+}
--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -1,8 +1,9 @@
-import React, { MutableRefObject, ReactNode, useCallback, useEffect, useRef, useState } from 'react'
+import { ReactNode } from 'react'
 import { useTheme } from 'shared/contexts'
-import { classNames, Mods } from 'shared/lib/classNames/classNames'
-import { Portal } from '../Portal/Portal'
+import { Mods, classNames } from 'shared/lib/classNames/classNames'
+import { useModal } from 'shared/lib/hooks/useModal/useModal'
 import { Overlay } from '../Overlay/Overlay'
+import { Portal } from '../Portal/Portal'
 import cls from './Modal.module.scss'
 
 interface ModalProps {
@@ -17,11 +18,12 @@ const ANIMATION_DELAY = 300
 
 export const Modal = (props: ModalProps) => {
     const { className, children, isOpen, lazy, onClose } = props
+    const { close, isClosing, isOpening, isMounted } = useModal({
+        animationDelay: ANIMATION_DELAY,
+        onClose,
+        isOpen,
+    })
 
-    const [isOpening, setIsOpening] = useState(false)
-    const [isClosing, setIsClosing] = useState(false)
-    const [isMounted, setIsMounted] = useState(false)
-    const timeRef = useRef() as MutableRefObject<ReturnType<typeof setTimeout>>
     const { theme } = useTheme()
 
     const mods: Mods = {
@@ -29,54 +31,14 @@ export const Modal = (props: ModalProps) => {
         [cls.isClosing]: isClosing,
     }
 
-    const closeHandler = useCallback(() => {
-        if (onClose) {
-            setIsClosing(true)
-            timeRef.current = setTimeout(() => {
-                onClose()
-                setIsClosing(prev => !prev)
-            }, ANIMATION_DELAY)
-        }
-    }, [onClose])
-
-    // Новые ссылки !!!
-    const onKeyDown = useCallback(
-        (e: KeyboardEvent) => {
-            if (e.key === 'Escape') closeHandler()
-        },
-        [closeHandler]
-    )
-
     // const onContentClick = (e: React.MouseEvent) => e.stopPropagation()
-
-    useEffect(() => {
-        if (isOpen) {
-            window.addEventListener('keydown', onKeyDown)
-            timeRef.current = setTimeout(() => {
-                setIsOpening(true)
-            }, 0)
-        }
-        return () => {
-            setIsOpening(false)
-            clearTimeout(timeRef.current)
-            window.removeEventListener('keydown', onKeyDown)
-        }
-    }, [isOpen, onKeyDown])
-
-    useEffect(() => {
-        if (isOpen) {
-            setIsMounted(true)
-        }
-        // При размонтировании - убираем Portal из DOM
-        return () => setIsMounted(false)
-    }, [isOpen])
 
     if (lazy && !isMounted) return null
 
     return (
         <Portal>
             <div className={classNames(cls.Modal, mods, [className, theme])}>
-                <Overlay onClick={closeHandler} />
+                <Overlay onClick={close} />
                 <div
                     className={cls.content}
                     // onClick={onContentClick}
