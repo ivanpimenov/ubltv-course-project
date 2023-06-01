83bbe2d55e8281d20ef179a9efd3b35a8fe78204
Ivan <ivan.pimenov@psylaris.com>
2023-06-01 17:03:32 +0200

    add feature flags concept

--- src/pages/ArticleDetailPage/ui/ArticleDetailPage/ArticleDetailPage.tsx
+++ src/pages/ArticleDetailPage/ui/ArticleDetailPage/ArticleDetailPage.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from 'react-i18next'
 import { useParams } from 'react-router-dom'
 
 import { ArticleDetails } from '@/entities/Article'
+import { Counter } from '@/entities/Counter'
 import { ArticleRating } from '@/features/articleRating'
 import { ArticleRecommendationsList } from '@/features/articleRecommendationsList'
 import { classNames } from '@/shared/lib/classNames/classNames'
@@ -10,6 +11,7 @@ import {
     DynamicModuleLoader,
     ReducersList,
 } from '@/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader'
+import { getFeatureFlag } from '@/shared/lib/features'
 import { VStack } from '@/shared/ui/Stack'
 import { Page } from '@/widgets/Page'
 
@@ -31,6 +33,8 @@ const ArticleDetailPage: FC<ArticleDetailPageProps> = (props) => {
     const { className } = props
     const { t } = useTranslation('article')
     const { id } = useParams<{ id: string }>()
+    const isArticleRatingEnabled = getFeatureFlag('isArticleRatingEnabled')
+    const isCounterEnabled = getFeatureFlag('isCounterEnabled')
 
     // if (!id) return <Page className={classNames(cls.ArticleDetailPage, {}, [className])}>{t('article not found')}</Page>
 
@@ -42,7 +46,10 @@ const ArticleDetailPage: FC<ArticleDetailPageProps> = (props) => {
                 <VStack gap='16' max>
                     <ArticleDetailsPageHeader />
                     <ArticleDetails id={id} />
-                    <ArticleRating articleId={id!} />
+                    {isCounterEnabled && <Counter />}
+                    {isArticleRatingEnabled && (
+                        <ArticleRating articleId={id!} />
+                    )}
                     <ArticleRecommendationsList />
                     <ArticleDetailsComments id={id!} />
                 </VStack>
